132e32e4ef31514ee7ff6d5b631d76636e0a7b08
CrackerCrypto <rajshybal10@gmail.com>
2024-02-07 17:01:35 +0530

    bucket name update01

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,12 @@
 terraform {
-  backend "s3" {
-    bucket = "gitcicdprojectbuket01"
-    key    = "terraform/backend"
-    region = "us-east-1"    
+    backend "s3" {
+        bucket = "gitcicdprojectbuket01"
+        key    = "terraform.tfstate"
+        region = "us-east-2"
   }
+
+  required_version = "~> 1.6.3"
 }
+##
+##
+##
