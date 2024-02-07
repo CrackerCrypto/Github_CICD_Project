0e530455127b618805526f8724e9887efcd1cc8a
CrackerCrypto <rajshybal10@gmail.com>
2024-02-07 17:58:17 +0530

    workflow no.11

---
+++ terraform/terraform.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.25.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.2"
+    }
+  }
+  backend "s3" {
+    bucket = "gitcicdprojectbuket01"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+  required_version = "~> 1.7.2"
+}
