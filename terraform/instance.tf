dc89264c89024f8c5812e3fb180cf463c6365f4b
CrackerCrypto <rajshybal10@gmail.com>
2024-02-07 16:28:44 +0530

    trying actions

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -2,7 +2,7 @@ resource "aws_key_pair" "github_cicd_key" {
   key_name   = "github_cicd_project"
   public_key = file("github_cicd_project.pub")
 }
-
+## Creates an instance
 resource "aws_instance" "github_cicd_pro" {
   ami                    = var.AMIS[var.REGION]
   instance_type          = "t2.micro"
