4127aab74aa192947a22d1e65d938002db919d68
CrackerCrypto <rajshybal10@gmail.com>
2024-02-07 21:59:57 +0530

    new commit no 6

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -6,11 +6,11 @@ terraform {
     }
     
   }
-  backend "s3" {
-    bucket = "gitcicdprojectbuket01"
-    key = "terraform.tfstate"
-    region = "us-east-1"
-  }
+  # backend "s3" {
+  #   bucket = "gitcicdprojectbuket01"
+  #   key = "terraform.tfstate"
+  #   region = "us-east-1"
+  # }
   required_version = "~> 1.7.2"
 }
 #/home/shybal/GithubDevops/terraform/.terraform/terraform.tfstate
